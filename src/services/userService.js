a653146ebfe94f62a77fbf1a7456a08860a99838
taewon1918 <taewon1918@gmail.com>
2025-08-19 09:34:00 +0900

    feed 등록 여러화면에서 가능하게 pinia로 처리

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,18 +2,18 @@ import axios from './httpRequester';
 
 const path = '/user';
 
-export const signUp = (args) => {
-  return axios.post(`${path}/sign-up`, args).catch((e) => e.response);
-};
+export const signUp = (data) => axios.post(`${path}/sign-up`, data);
 
-export const signIn = (args) => {
-  return axios.post(`${path}/sign-in`, args).catch((e) => e.response);
-};
+export const signIn = (data) => axios.post(`${path}/sign-in`, data);
 
-export const signOut = () => {
-  return axios.post(`${path}/sign-out`).catch((e) => e.response);
-};
+export const signOut = () => axios.post(`${path}/sign-out`);
 
-export const reissue = () => {
-  return axios.post(`${path}/reissue`).catch((e) => e.response);
-};
+export const reissue = (data) => axios.post(`${path}/reissue`, data);
+
+export const getUserProfile = (params) =>
+  axios.get(`${path}/profile`, { params });
+
+export const patchUserProfilePic = (data) =>
+  axios.patch(`${path}/profile/pic`, data);
+
+export const deleteUserProfilePic = () => axios.delete(`${path}/profile/pic`);
